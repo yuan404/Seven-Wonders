2fdb308f262471b7bc9202ff99ef148fb6b44d11
Lane <lanelim555@gmail.com>
2014-09-28 23:13:08 +0800

    V-0.3.2

--- src/Kernel/Card.java
+++ src/Kernel/Card.java
@@ -1,10 +1,12 @@
 package Kernel;
 
+import javafx.scene.image.ImageView;
+
 public class Card extends BuildingItem {
 	/**
 	 * 免费建造列表
 	 */
-	public String[] freeBuild = new String[2];
+	private String[] freeBuild = new String[2];
 	/**
 	 * 添加至免费列表
 	 * @param name
@@ -14,5 +16,65 @@ public class Card extends BuildingItem {
 			freeBuild[0] = new String(name);
 		else
 			freeBuild[1] = new String(name);
+	}
+	/**
+	 * 卡牌名称
+	 */
+	private String name;
+	/**
+	 * 卡牌颜色
+	 */
+	private String color;
+	/**
+	 * 卡牌时代
+	 */
+	private int age;
+	/**
+	 * 卡牌开启人数
+	 */
+	private int num;
+	/**
+	 * 构造函数
+	 * @param name
+	 * @param color
+	 * @param age
+	 * @param num
+	 */
+	public Card(String name, String color, int age, int num) {
+		this.name = name;
+		this.color = color;
+		this.age = age;
+		this.num = num;
+	}
+	/**
+	 * 卡牌模型
+	 */
+	public ImageView iv = new ImageView();
+	/**
+	 * 由卡牌得到名称
+	 */
+	public String getName(){
+		return name;
+	}
+	/**
+	 * 由卡牌得到颜色
+	 * @return
+	 */
+	public String getColor(){
+		return color;
+	}
+	/**
+	 * 由卡牌得到时代数
+	 * @return
+	 */
+	public int getAge(){
+		return age;
+	}
+	/**
+	 * 由卡牌得到人数限制
+	 * @return
+	 */
+	public int getNum(){
+		return num;
 	}
 }
